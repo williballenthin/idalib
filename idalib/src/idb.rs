1ec93cb88ed7c7a50a76197fcdd3bfa1226554eb
Willi Ballenthin <wballenthin@hex-rays.com>
2025-11-04 14:43:36 +0100

    add imagebase accessor

--- idalib/src/idb.rs
+++ idalib/src/idb.rs
@@ -207,6 +207,10 @@ impl IDB {
         MetadataMut::new()
     }
 
+    pub fn imagebase(&self) -> Address {
+        self.meta().imagebase()
+    }
+
     pub fn processor(&self) -> Processor<'_> {
         let ptr = unsafe { get_ph() };
         Processor::from_ptr(ptr)
