6a45f2ea6dd7d52a8f8a8234d479d3d5750a93df
Willi Ballenthin <wballenthin@hex-rays.com>
2026-02-01 06:44:08 +0100

    tests: remove old IO routines

--- idalib/src/tests.rs
+++ idalib/src/tests.rs
@@ -1,5 +1,4 @@
-use std::io::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 /// Fetch the file system path of the given test file.
 ///
@@ -12,18 +11,4 @@ pub fn get_test_file_path(filename: &str) -> PathBuf {
     d.push("tests");
     d.push(filename);
     d
-}
-
-fn read_file(path: &Path) -> Vec<u8> {
-    let mut buf = Vec::new();
-    {
-        let mut f = std::fs::File::open(&path).unwrap();
-        f.read_to_end(&mut buf).unwrap();
-    }
-    buf
-}
-
-pub fn get_test_file_buf(filename: &str) -> Vec<u8> {
-    let path = get_test_file_path(filename);
-    read_file(&path)
 }
